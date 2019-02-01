c2e3558bd2183e90950c2d07feb58bb6171f432c
unknown <chrisduroseau@yahoo.com>
2019-01-31 20:36:54 -0500

    flipcard component

---
+++ therunwayshop/src/components/flipCard/card.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './card.css'
+import {TweenMax, TimelineMax} from 'gsap'
+
+
+class Card extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.tween = null
+      }
+
+    componentDidMount() {
+        TweenMax.set(this.backCard, { rotationY: -180 });
+
+    this.tween = new TimelineMax({ paused: true });
+
+    this.tween
+      .to(this.frontCard, 1, { rotationY: 180 })
+      .to(this.backCard, 1, { rotationY: 0 }, 0)
+      .to(this.element, .5, { z: 50 }, 0)
+      .to(this.element, .5, { z: 0 }, .5);
+
+    this.element.animation = this.tween;
+
+    }
+
+    onMouseOver = () => {
+        this.tween.play()
+    }
+
+    onMouseLeave = () => {
+        this.tween.reverse()
+    }
+
+    render() {
+        return (
+        <div ref={c => this.element = c} class="cardCont" onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseLeave}>
+            <div class="cardBack" ref={d => this.frontCard = d} style={{background:`url(${this.props.backImg})`}}></div>
+            <div class="cardFront" ref={thing => this.backCard = thing} style={{background:`url(${this.props.frontImg})`}}></div>
+          </div>
+        )
+    }
+}
+
+
+export default Card;
--- therunwayshop/src/components/pages/Home.js
+++ therunwayshop/src/components/pages/Home.js
@@ -1,14 +1,57 @@
 import React from 'react'
+import Card from '../flipCard'
 
-function Home() {
-  return (
-    <div>
-      <h1> Home Page</h1>
-      <p>
-        This page will be the first page a user sees when visiting the site
-      </p>
-    </div>
-  );
+
+class Home extends React.Component {
+
+  state={
+    CardList: [
+      {
+        frontImageURL: "https://s.cdpn.io/33073/lorempixe1l.jpg",
+        backImageURL: "http://unlimitedpotentialnow.com/wp-content/uploads/2012/01/Bouquet-cs.jpg"
+      },
+      {
+        frontImageURL: "https://s.cdpn.io/33073/lorempixe1l.jpg",
+        backImageURL: "http://unlimitedpotentialnow.com/wp-content/uploads/2012/01/Bouquet-cs.jpg"
+      },
+      {
+        frontImageURL: "https://s.cdpn.io/33073/lorempixe1l.jpg",
+        backImageURL: "http://unlimitedpotentialnow.com/wp-content/uploads/2012/01/Bouquet-cs.jpg"
+      },
+      {
+        frontImageURL: "https://s.cdpn.io/33073/lorempixe1l.jpg",
+        backImageURL: "http://unlimitedpotentialnow.com/wp-content/uploads/2012/01/Bouquet-cs.jpg"
+      },
+
+    ]
+  }
+
+  componentDidMount() {
+    //do axios call to DB and get all card images.
+    //in callback set this to the cardList array
+  }
+
+  render() {
+
+    return (
+
+      <div>
+        <div id="mainWrap">
+        {
+          this.state.CardList.map((item, key) => {
+            return(
+              <Card key={key} frontImg={item.frontImageURL} backImg={item.backImageURL} />
+            )
+          })
+        }
+
+          
+        </div>
+      </div>
+
+    );
+  }
+
 }
 
 export default Home
