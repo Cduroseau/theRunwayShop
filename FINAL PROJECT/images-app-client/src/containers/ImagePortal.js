a1e9e8b9ba884dedbea6f3ed5e0d9a225694c7df
unknown <chrisduroseau@yahoo.com>
2019-02-11 13:20:59 -0500

    2nd Version

--- FINAL PROJECT/images-app-client/src/containers/ImagePortal.js
+++ FINAL PROJECT/images-app-client/src/containers/ImagePortal.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
 import "./ImagePortal.css";
 
-export default class ImagePortal extends Component {
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      images: []
+    };
+  }
+
+  renderNotesList(Images) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="images">
+        <PageHeader>Image Portal</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="ImagePortal">
-        <div className="lander">
-          <h1>The Runway</h1>
-          <p>This page is the Image Portal, where all the images will be displayed</p>
-        </div>
+        {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
       </div>
     );
   }
